8487da2ae0ba71968713cf1ee82ce8fc485b89e6
Dor Mull-or <dormullor@gmail.com>
2022-11-08 17:10:45 +0200

    First init

--- util/util.go
+++ util/util.go
@@ -2,7 +2,7 @@ package util
 
 import (
 	"bytes"
-	"crypto/sha1"
+	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 	"net"
@@ -218,20 +218,20 @@ func getThumbprint(oidcEndpointURL string) (string, error) {
 	for _, cert := range certChain {
 		if cert.IsCA {
 			if bytes.Equal(cert.RawIssuer, cert.RawSubject) {
-				return sha1Hash(cert.Raw), nil
+				return sha256Hash(cert.Raw), nil
 			}
 		}
 	}
 
 	// Fall back to using the last certficiate in the chain
 	cert := certChain[len(certChain)-1]
-	return sha1Hash(cert.Raw), nil
+	return sha256Hash(cert.Raw), nil
 }
 
 // sha1Hash computes the SHA1 of the byte array and returns the hex encoding as a string.
-func sha1Hash(data []byte) string {
+func sha256Hash(data []byte) string {
 	// nolint:gosec
-	hasher := sha1.New()
+	hasher := sha256.New()
 	hasher.Write(data)
 	hashed := hasher.Sum(nil)
 	return hex.EncodeToString(hashed)
